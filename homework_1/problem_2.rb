ad6893b2c0cb6fd4c2c0081b37641361c929eb38
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-18 00:03:10 -0400

    renamed homework_1 directory and added code to problem_2

---
+++ homework_1/problem_2.rb
@@ -0,0 +1,11 @@
+# Problem 2
+# Write a method called add that takes two parameters, adds them together, and returns the result. For example, add(1, 2) should return 3.
+
+# Then, add a line that calls your add method with two numbers of your choosing, stores the result in a variable with a name of your choosing, and then prints the result to the screen.
+
+def add(a, b)
+	a + b
+end
+
+result = add(1, 2)
+puts(result)
