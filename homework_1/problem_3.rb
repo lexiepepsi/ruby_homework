6c860d9c31c830b535c29a25dc04400030b8bae8
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-18 00:29:02 -0400

    added code to problem_3

---
+++ homework_1/problem_3.rb
@@ -0,0 +1,15 @@
+# Problem 3
+# Write a method called join_strings that takes two string values, joins them together with a space in between, and returns the result. For example, join_strings('Hello', 'Dolly!') should return "Hello Dolly!".
+
+def join_strings(a, b)
+	if (a.is_a? (String)) && (b.is_a? (String))
+		puts(a + " " + b)
+	else
+		puts("ruh roh, try again")
+	end
+end
+
+# puts(join_strings("hello", "dolly!"))
+
+join_strings("hello", "dolly!")
+join_strings(1, "dolly!")
