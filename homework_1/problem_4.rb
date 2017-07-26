b3c76d062e0d5c5fe0365020376b0db987437c68
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-26 02:01:21 -0400

    fixed prob 4 from homework 1 and began prob 1 from homework 2

--- homework_1/problem_4.rb
+++ homework_1/problem_4.rb
@@ -6,7 +6,7 @@
 # Answer:
 # Puts is a predefined method in Ruby that prints something out to the terminal screen. It makes something visable but it does not change the value of what it shows or the value of the method puts itself. The return of puts is always nil.
 
-# For examplke, in the add method below we put out the value of a, then we add a + b. 
+# For example, in the add method below we put out the value of a, then we add a + b. 
 # Puts will print the value of a to the terminal screen but it does not change the return value of puts and it does not change the returned value of the add method. The returned value of the add method is the last line of the method, a + b. While this add method will dispaly a to the terminal screen when we run it, the returned value of the add method is actually a + b.
 def add(a, b)
 	puts(a)
@@ -16,12 +16,12 @@ end
 # For example, if I run add(1,2):
 # add(1,2)
 
-# The result will look like this:
+# The result will look like this when running irb in terminal:
 # >1
 # => 3
 # Here, 1 prints to the terminal screen but the returned value of the add method is 3
 
 
-# If we want to see the returned value of the add method we can use puts on the method itself to puts or print the returned value of the method. In this code I define a variable called result that takes the add method with an argument of (1,2). Then I puts or print out the return value of result so we can see the return of the add method given the argument (1,2). Result should cause add to return three and the puts will show our returned value of 3 to the terminal screen.
+# To see the returned value of the add method we can use puts on the method itself to puts or print the returned value of the method. In this code I define a variable called result that takes the add method with an argument of (1,2). The returned value of add(1,2), or 3, will be stored in the result variable. Then I puts or print out result so we can see 3 (the returned value of add(1,2)) on our screeen.
 result = add(1, 2)
 puts(result)
--- homework_2/problem_1.rb
+++ homework_2/problem_1.rb
@@ -3,20 +3,35 @@
 # Add a comment at the beginning of the following code describing in plain English what it does in general, then add a comment before each line explaining what that particular line does specifically.
 
 
-# General Description: 
+# General Description: The fizzbuzz method checks if a number fed in as its argument is a multiple of 15, 3, or 5. It divides the argument by 15, 3, and/or 5 and checks if the remainder of the division equals 0. If the remaineder = 0, the fizzbuzz method returns a specific string for each number, and if the remainder is no 0 the method returns the argument number itself.
+
+# The fizzbuzz_to method takes a number as an argument which serves as the limit or last number the method will act on. For each number, starting from 1 and going up to the argument-number, fizzbuzz_to will run fizzbuzz with that number as the argument and print out the result to the screen. When we reach the argument limit number the method ends the action.
 
 
+# Create a method called fizzbuzz that takes an argument caleld number.
 def fizzbuzz(number)
+  #?????	
   case
+  	# Anytime the argument number divides by 15 with a remainder equal to 0, return the string "Fizzbuzz".
     when number % 15 == 0 then 'Fizzbuzz'
+    # Anytime the argument number divides by 3 with a remainder equal to 0, return the string "Fizz".
     when number % 3  == 0 then 'Fizz'
+    # Anytime the argument number divides by 5 with a remainder equal to 0, return the string "Buzz".
     when number % 5  == 0 then 'Buzz'
+    # Otherwise, that is if the argument number never divided by 15, 3, or 5 with a remainder equal to 0, return the argument number.
     else number
+  # Ends the case clause.
   end
+# Ends defining the creation of the fizzbuzz method.
 end
 
+# Create a method called fizzbuzz_to that takes an argument caleld limit.
 def fizzbuzz_to(limit)
+  # Start with the number 1 and go up to the argument number. For each number, from 1 to the argument number ???????? put the number in a code blocK?
   1.upto(limit) do |i|
+  	# Print out the returned value of the fizzbuzz method with feeding in i as the argument.
     puts(fizzbuzz(i))
+  # Ends the code block do????
   end
+# Ends defining the creation of the fizzbuzz_to method.
 end
