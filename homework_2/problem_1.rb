0bbfa925cc4c758e405745faef02e2fe4cf13a41
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-28 01:06:46 -0400

    fized typos in prob 1

--- homework_2/problem_1.rb
+++ homework_2/problem_1.rb
@@ -3,7 +3,7 @@
 # Add a comment at the beginning of the following code describing in plain English what it does in general, then add a comment before each line explaining what that particular line does specifically.
 
 
-# General Description: The fizzbuzz method checks if a number fed in as its argument is a multiple of 15, 3, or 5. It does this by dividing the argument integer by 15, 3, and/or 5 and checking if the remainder of the division equals 0. If the remaineder = 0, the fizzbuzz method returns a specific string for each number to indicate the number is a multiple of either 15, 3, or 5. If the remainder is not 0 the method returns the argument number itself, indicating the argument is not a multiple of 15, 3, or 5.
+# General Description: The fizzbuzz method checks if a number fed in as its argument is a multiple of 15, 3, or 5. It does this by dividing the argument integer by 15, 3, and/or 5 and checking if the remainder of the division equals 0. If the remainder = 0, the fizzbuzz method returns a specific string for each number to indicate the number is a multiple of either 15, 3, or 5. If the remainder is not 0 the method returns the argument number itself, indicating the argument is not a multiple of 15, 3, or 5.
 
 # The fizzbuzz_to mehtod prints out the results of fizzbuzz so we can see which numbers are multiples of 15, 3, and 5. The fizzbuzz_to method takes a number as an argument which serves as the limit or last number the method will act on. For each number, starting from 1, going up to (and including) the argument-number, fizzbuzz_to will run fizzbuzz with that number as the argument and print out the result to the screen. After the argument limit number has run the method ends the action.
 
@@ -13,7 +13,7 @@ def fizzbuzz(number)
   # Create a case statement. (Case statements test equality using 'when' clauses. The first 'when' clause to evaluate to true gets its code executed.)
   case
   	# When the argument number divides by 15 with a remainder equal to 0, return the string "Fizzbuzz". 
-    # ((Note, 15 comes before 5. Since the when statments are evaluated top-down a number such as 30, which is a multiple of all 3 numbers, will return Fizzbuzz because the first when statement will evaluate to true and get its code executed before we even get to 3 or 5 in the lines below it.))
+    # ((Note, 15 comes before 5. Since the when statements are evaluated top-down a number such as 30, which is a multiple of all 3 numbers, will return Fizzbuzz because the first when statement will evaluate to true and get its code executed before we even get to 3 or 5 in the lines below it.))
     when number % 15 == 0 then 'Fizzbuzz'
     # When the argument number divides by 3 with a remainder equal to 0, return the string "Fizz".
     when number % 3  == 0 then 'Fizz'
