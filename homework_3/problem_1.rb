1abe9592fa36423c7e1d69e52a09a4aaefa9d002
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-29 00:19:37 -0400

    created homework 3 files, first commit on them

---
+++ homework_3/problem_1.rb
@@ -0,0 +1,5 @@
+# Problem 1
+# Create a class called Utility.
+# Add a class method to Utility called count_words that takes a string as a parameter. The method should return a hash where the keys are words and the values are numbers indicating the number of times the key occurred in the input string. For example, Utility.count_words('the good and the bad and the ugly') should return:
+# {"the" => 3, "good" => 1, "and" => 2, "bad" => 1, "ugly" => 1}
+# Print the result of calling this method with the string it was the best of times, it was the worst of times.
