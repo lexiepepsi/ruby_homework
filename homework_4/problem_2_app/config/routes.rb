866ac0a42853b1193c39f2d4db60e0d1879f2978
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-08-05 00:44:44 -0400

    finished form and displaying form input on thanks page

--- homework_4/problem_2_app/config/routes.rb
+++ homework_4/problem_2_app/config/routes.rb
@@ -4,10 +4,3 @@ Rails.application.routes.draw do
   post('thanks' => 'home#thanks')
 
 end
-
-
-# Create a page accessible at the URL /contact that contains a form with fields for a name, email, and message.
-
-# Upon submission of the contact form, bring the user to a page at the URL /thanks that displays the name, email, and message that was entered on the submitted form.
-
-# Create a homepage accessible at the root URL of the site (that is, http://localhost:3000) that contains a link to the contact page.
