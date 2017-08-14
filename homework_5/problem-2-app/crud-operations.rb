f6724531c7b8301abe2e01206209f08f01a156d6
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-08-13 23:27:13 -0400

    created file to show curd operations practiced

---
+++ homework_5/problem-2-app/crud-operations.rb
@@ -0,0 +1,46 @@
+user = User.create(first_name: "Alexa", last_name: "Kovachevich", email: "alexa@barkbox.com", address: "200 Water Street", password: "poop")
+
+User.all
+
+user2 = User.new(first_name: "Joyce", last_name: "Kovachevich") 
+user2.save
+
+User.find(2)
+
+User.first
+User.last
+
+User.where(first_name: "Alexa")
+
+User.last.update(email: "jkovachev@comcast.net")
+
+user2.last_name = "Kovachevich"
+user2.save
+
+User.create(first_name: "test", last_name: "testttt")
+User.last.destroy
+
+User.create(first_name: "Ned", last_name: "Kovachevich")
+User.find(4).delete
+
+user6 = User.new(first_name: "Arianna")
+user6.new_record?
+
+user6.destroyed?
+
+user6.update(last_name: "Kovachevich")
+
+User.limit(2)
+User.offset(2)
+
+User.exists?(first_name: "Alexa")
+
+User.find(1, 2, 5)
+
+User.count
+
+User.ids
+
+User.pluck(:id)
+User.pluck(:first_name)
+User.pluck(:first_name, :last_name)
