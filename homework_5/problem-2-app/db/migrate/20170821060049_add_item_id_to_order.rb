356b710cd23ed3701eec1c138b12c76ff96e33f3
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-08-27 19:36:10 -0500

    added styling changed order form

--- homework_5/problem-2-app/db/migrate/20170821060049_add_item_id_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddItemIdToOrder < ActiveRecord::Migration
-  def change
-  	add_column :orders, :item_id, :integer
-  end
-end
--- homework_5/problem-2-app/db/schema.rb
+++ homework_5/problem-2-app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821060049) do
+ActiveRecord::Schema.define(version: 20170814024002) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "sku"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20170821060049) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "item_id"
   end
 
   create_table "users", force: :cascade do |t|
