f556e9acf917cc9a7460f5c8e1307b3ae902a61e
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-08-28 20:22:39 -0400

    Homework 7.

---
+++ homework_7/prob-1-app/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(params[:user].permit(:name, :email))
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(params[:user].permit(:name, :email))
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+	end
+end
