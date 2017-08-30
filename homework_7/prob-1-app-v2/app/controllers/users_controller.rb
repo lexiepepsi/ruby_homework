4bf350bd1c3c1634be76427ca3b1a4d5798b06df
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-08-30 00:27:49 -0400

    added methods to users controller and more views

--- homework_7/prob-1-app-v2/app/controllers/users_controller.rb
+++ homework_7/prob-1-app-v2/app/controllers/users_controller.rb
@@ -2,8 +2,31 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(params[:user].permit(:name, :email))
+	end
 	
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(params:[:user].permit(:name, :email))
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+	end
+
 end
