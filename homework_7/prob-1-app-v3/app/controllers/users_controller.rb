bc6bd03a6f341fadd4be21ea23ec51ee406207bd
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 10:53:03 -0400

    fixed styling

--- homework_7/prob-1-app-v3/app/controllers/users_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+	# before_action(:authenticate_user!)
 	before_action(:authenticate_user!, except: [:index, :show])
-	
+
+
 	def index
 		@users = User.all
 	end
