e4d35521ac440ddfc9b46260f30edcfce692d181
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 11:53:47 -0400

    added find-user method to controller

--- homework_7/prob-1-app-v3/app/controllers/users_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	# before_action(:authenticate_user!)
 	before_action(:authenticate_user!, except: [:index, :show])
+	before_action(:find_user, only: [:show, :edit, :update, :destroy])
 
 
 	def index
@@ -16,20 +17,27 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 		@user.update(params[:user].permit(:name, :profile, :headline))
 	end
 
 	def destroy
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 		@user.destroy
 	end
+
+	private
+
+	def find_user
+		@user = User.find(params[:id])
+	end
+
 end
