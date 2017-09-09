bc0692db6f1f8178175fbc714b225d3e9f869e7e
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 12:56:30 -0400

    added info to app controller so name profile and headline are part of user devise sign up form and linked to show page for users name on index

--- homework_7/prob-1-app-v3/app/controllers/users_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/users_controller.rb
@@ -2,35 +2,35 @@ class UsersController < ApplicationController
 	# before_action(:authenticate_user!)
 	before_action(:authenticate_user!, except: [:index, :show])
 	before_action(:find_user, only: [:show, :edit, :update, :destroy])
-
-
 	def index
 		@users = User.all
 	end
 
-	def new
-		@user = User.new
-	end
+	# def new
+	# 	@user = User.new
+	# end
 
-	def create
-		@user = User.create(params[:user].permit(:name, :profile, :headline))
-	end
+	# def create
+	# 	@user = User.create(params[:user].permit(:name, :profile, :headline))
+	# end
 
 	def show
-		# @user = User.find(params[:id])
 	end
 
 	def edit
-		# @user = User.find(params[:id])
 	end
 
 	def update
-		# @user = User.find(params[:id])
 		@user.update(params[:user].permit(:name, :profile, :headline))
+
+		if @user.valid?
+			redirect_to_user('Your account was sucessfully updated!')
+		else
+			render(:edit)
+		end
 	end
 
 	def destroy
-		# @user = User.find(params[:id])
 		@user.destroy
 	end
 
@@ -39,5 +39,10 @@ class UsersController < ApplicationController
 	def find_user
 		@user = User.find(params[:id])
 	end
+
+	def redirect_to_user(message)
+		redirect_to(@user, notice: message)
+	end
+
 
 end
