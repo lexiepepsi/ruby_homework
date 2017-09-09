83a29862de4856a05410dbfb8917eaebb9f3e89a
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 19:04:41 -0400

    updated edit and delete so they dont show when user is nil

--- homework_7/prob-1-app-v3/app/controllers/users_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action(:authenticate_user!, except: [:index])
+	before_action(:authenticate_user!, except: [:index, :show])
 	before_action(:find_user, only: [:show, :edit, :update, :destroy])
 	before_action(:redirect_to_homepage_unless_user_belongs_to_current_user, only: [:edit, :update])
 
