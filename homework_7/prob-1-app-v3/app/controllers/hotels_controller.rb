7221997febc3a7dac658c267218c0c6e1a2eca4f
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 20:49:27 -0400

    added views and links for hotel model

--- homework_7/prob-1-app-v3/app/controllers/hotels_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/hotels_controller.rb
@@ -1,2 +1,32 @@
 class HotelsController < ApplicationController
+
+	def index
+		@hotels = Hotel.all
+	end
+
+	def new
+		@hotel = Hotel.new
+	end
+
+	def create
+		@hotel = Hotel.create(params[:hotel].permit(:name, :description))
+	end
+
+	def show
+		@hotel = Hotel.find(params[:id])
+	end
+
+	def edit
+		@hotel = Hotel.find(params[:id])
+	end
+
+	def update
+		@hotel = Hotel.find(params[:id])
+		@hotel.update(params[:hotel].permit(:name, :description))
+	end
+
+	def destroy
+		@hotel = Hotel.find(params[:id])
+		@hotel.destroy
+	end
 end
