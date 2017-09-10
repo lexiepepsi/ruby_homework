2a1ceed0cf22f22277ae2bcef43acf2f9b0d18e1
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 23:50:16 -0400

    added edit and delete on hotel show page only for admins and current user

--- homework_7/prob-1-app-v3/app/controllers/hotels_controller.rb
+++ homework_7/prob-1-app-v3/app/controllers/hotels_controller.rb
@@ -1,5 +1,8 @@
 class HotelsController < ApplicationController
 	before_action(:authenticate_user!, except: [:index, :show])
+	before_action(:find_hotel, only: [:show, :edit, :update, :destroy])
+	before_action(:redirect_to_homepage_unless_hotel_belongs_to_current_user, only: [:edit, :update])
+
 
 	def index
 		@hotels = Hotel.all
@@ -11,23 +14,48 @@ class HotelsController < ApplicationController
 
 	def create
 		@hotel = Hotel.create(params[:hotel].permit(:name, :description, :user_id))
+
+		if @hotel.valid?
+			redirect_to_hotel('Sucessfully created!')
+		else
+			render(:new)
+		end
 	end
 
 	def show
-		@hotel = Hotel.find(params[:id])
 	end
 
 	def edit
-		@hotel = Hotel.find(params[:id])
 	end
 
 	def update
-		@hotel = Hotel.find(params[:id])
 		@hotel.update(params[:hotel].permit(:name, :description, :user_id))
+
+		if @hotel.valid?
+			redirect_to_hotel('Sucessfully updated!')
+		else
+			render(:edit)
+		end
 	end
 
 	def destroy
-		@hotel = Hotel.find(params[:id])
 		@hotel.destroy
 	end
+
+	private
+
+	def find_hotel
+		@hotel = Hotel.find(params[:id])
+	end
+
+	def redirect_to_hotel(message)
+		redirect_to(@hotel, notice: message)
+	end
+
+	def redirect_to_homepage_unless_hotel_belongs_to_current_user
+		unless @hotel.admin_or_belongs_to?(current_user, @hotel.id)
+			redirect_to(hotels_path, notice: 'That is not yours!')
+		end
+	end
+
 end
--- homework_7/prob-1-app-v3/app/models/hotel.rb
+++ homework_7/prob-1-app-v3/app/models/hotel.rb
@@ -1,3 +1,10 @@
 class Hotel < ActiveRecord::Base
 	belongs_to(:user)
+
+	validates_presence_of(:name, :description, :user_id)
+
+	def admin_or_belongs_to?(person, place)
+		person.try(:admin?) || person.hotels.ids.include?(place)
+	end
+
 end
