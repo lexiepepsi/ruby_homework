46d712fe90c53e677efd0b9d712d49d542f367d3
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-09-09 20:11:30 -0400

    added hotel model and relationships btw hotel and users

--- homework_7/prob-1-app-v3/db/schema.rb
+++ homework_7/prob-1-app-v3/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909150139) do
+ActiveRecord::Schema.define(version: 20170909235709) do
+
+  create_table "hotels", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
